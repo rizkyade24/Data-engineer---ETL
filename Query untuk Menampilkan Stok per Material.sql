94c363fb5d7384747124509e746bc054d9432b94
rizky ade <76718182+rizkyade24@users.noreply.github.com>
2024-12-30 07:37:19 +0700

    Delete Query untuk Menampilkan Stok per Material.sql

--- Query untuk Menampilkan Stok per Material.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-    dm.material_name,
-    SUM(fs.qty) AS total_stock
-FROM 
-    fact_stock fs
-JOIN 
-    dim_material dm ON fs.material_id = dm.material_id
-GROUP BY 
-    dm.material_name;
